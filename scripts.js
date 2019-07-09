672a85fd2a6d721dc47a7e5761c10b462e3f5efa
Kefa Mutuma <mutmakeffa@gmail.com>
2019-07-10 00:01:18 +0300

    changes

--- scripts.js
+++ scripts.js
@@ -5,12 +5,6 @@ document.addEventListener("DOMContentLoaded", () => {
 	let btn = document.querySelector("button")  
 	btn.onclick = changeBackground;
 
-	// function changeBackground () {
-	// 	document.body.style.background = "#222f3e";
-	// 	document.body.style.color = 'white';
-	// 	btn.style.backgroundColor = '#e85130';
-	// 	btn.style.color = 'yellow';
-
 	function changeBackground() {
 		var element = document.getElementById("myDIV");
     	element.classList.toggle("mystyle");
